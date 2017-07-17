9f1686aa094ffe77270099278c0f95d594a5fb1c
Aja Hammerly <ajahammerly@google.com>
2017-07-17 15:58:45 -0700

    Switched how the board is represented in memory.

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,4 @@
-# Copyright 2017 Google
+# Copyright 2017 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,7 +20,7 @@ class Board
   def initialize
     self.height = 10
     self.width  = 10
-    @board  = Hash.new(".")
+    @board  = Hash.new { |h, k| h[k] = [] }
   end
 
   def in_range? location
@@ -34,28 +34,38 @@ class Board
   end
 
   def [] location
-    @board[location]
+    @board.each do |value, locations|
+      return value if locations.include?(location)
+    end
+    return "."
   end
 
   def []= location, value
-    @board[location] = value
+    @board[value] << location
   end
 
   def hit? location
-    @board[location] != "."
+    @board.values.any? { |locations| location.include?(location) }
   end
 
   def empty? location
-    @board[location] == "."
+    @board.values.none? { |locations| locations.include?(location) }
   end
 
   alias_method :miss?, :empty?
 
   def to_s
+    inverted = Hash.new(".")
+    @board.each do |ship, locations|
+      locations.each do |l|
+        inverted[l] = ship
+      end
+    end
+
     str = ""
 
     str = LETTERS.map { |l|
-      NUMBERS.map { |n| @board["#{l}#{n}"][0] }.join
+      NUMBERS.map { |n| inverted["#{l}#{n}"][0] }.join
     }.join("\n")
     str << "\n"
 
