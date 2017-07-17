6b41cffc7b343a73409d64d09fbb045df29c4eef
Aja Hammerly <ajahammerly@google.com>
2017-07-17 14:04:48 -0700

    Add license files

---
+++ README.md
@@ -0,0 +1,15 @@
+## Synopsis
+
+This is a server and clients that implement the game Battleship. The goals are
+* Explore client/server patterns in different languages
+* Demonstrate different ways to deploy an application on GCP
+* Demonstrate different ways to monitor applications and address issues
+
+## Contributing
+
+I welcome versions of this project written in other languages. To contribute see CONTRIBUTING.md 
+
+
+## License
+
+Licensed Apache V2 (see licensing file)
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,16 @@
+# Copyright 2017 Google
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#      http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'securerandom'
 require_relative 'client'
 
@@ -15,5 +28,20 @@ class Game
     @id = SecureRandom.uuid
     @client_a = Client.new(@id, FLEET)
     @client_b = Client.new(@id, FLEET)
+  end
+
+  def run
+    @client_a.place_ships
+    @client_b.place_ships
+
+    loop do
+      g = @client_a.guess
+      _ = @client_b.process_guess
+
+      break if @client_a.lost? or @client_b.lost?
+
+      g = @client_b.guess
+      _ = @client_a.process_guess
+    end
   end
 end
